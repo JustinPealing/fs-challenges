33e8f4bc86711fb26c077646319b12e209ffa05e
Justin Pealing <justin@pealing.me.uk>
2019-03-25 22:18:45 +0000

    mstest + console app

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let test =
+    "Hello world"
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass() =
+    [<TestMethod>]
+    member this.TestMethodPassing() =
+        Assert.AreEqual(test, "Hello world")
+
+[<EntryPoint>]
+let main argv =
+    printfn "%s" test
+    0
