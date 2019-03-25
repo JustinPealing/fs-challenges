101b5609f8cbb5ef11b81b61dced179997d95cf4
Justin Pealing <justin@pealing.me.uk>
2019-03-25 23:27:48 +0000

    Complete

--- Program.fs
+++ Program.fs
@@ -6,10 +6,16 @@ let canTake (queenx, queeny) (otherx, othery) =
     else if queenx - queeny = otherx - othery then true
     else queenx + queeny = otherx + othery
 
+let isAttacking others queen = 
+    Seq.exists (canTake queen) others
+
+let rec attacking queens =
+    Seq.where (fun q -> Seq.exists (fun x -> x <> q && canTake x q) queens) queens
+
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
-type TestClass() =
+type CanTakeTests() =
     [<TestMethod>]
     member this.CanTakeOnRank() =
         Assert.IsTrue(canTake (1, 1) (1, 5))
@@ -29,8 +35,36 @@ type TestClass() =
     member this.CannotTakeWhenInvalid() =
         Assert.IsFalse(canTake (1, 1) (2, 3))
         Assert.IsFalse(canTake (3, 1) (4, 3))
+        
+[<TestClass>]
+type AttackingTests() =
+    let sequenceEqual a b =
+        Seq.fold (&&) true (Seq.zip a b |> Seq.map (fun (aa,bb) -> aa=bb))
+
+    [<TestMethod>]
+    member this.EmptyList() =
+        let result = attacking []
+        Assert.AreEqual(0, Seq.length result)
+
+    [<TestMethod>]
+    member this.CannotTake() =
+        let result = attacking [(1, 1); (3, 4)]
+        Assert.AreEqual(0, Seq.length result)
+
+    [<TestMethod>]
+    member this.CanTake() =
+        let queens = [(1, 1); (2, 2)]
+        sequenceEqual queens (attacking queens) |> Assert.IsTrue
+
+    [<TestMethod>]
+    member this.Example() =
+        let queens = [(2,1); (4,3); (6,3); (8,4); (3,4); (1,6); (7,7); (5,8)]
+        let expected = [(2,1); (4,3); (6,3); (8,4); (3,4); (1,6)]
+        sequenceEqual expected (attacking queens) |> Assert.IsTrue
 
 [<EntryPoint>]
 let main argv =
-    printfn "%s" test
+    let queens = [(2,1); (4,3); (6,3); (8,4); (3,4); (1,6); (7,7); (5,8)]
+    let result = attacking queens |> Seq.toList
+    printfn "%A" result
     0
